5db197cf5981c8e356b8486e48a5b2c65421694b
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-03-03 16:51:37 +0600

    responsive login and reg

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import loginAnimation from "./68312-login.gif";
 import { Link } from "react-router-dom";
+import { inputData } from "../../fakeData/inputData/inputData";
 const Login = () => {
   return (
     <div>
@@ -15,32 +16,32 @@ const Login = () => {
             </div>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="email"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
-                <label className="label">
-                <Link className="label-text-alt link link-hover">
+            <div className="card-body p-10">
+            {
+                    inputData?.map((value,index)=>{
+                        return    <div className="form-control" key={index}>
+                        {
+                            value.id !== 1 ?  <div className="flex p-2 items-center border rounded-md">
+                            <div className="icon">
+                               {value.icon}
+                            </div>
+                            <div className="in">
+                            <input
+                          type={value.name}
+                          placeholder={value.placeholder}
+                          className="input focus:outline-none"
+                        />
+                            </div>
+                        </div> : undefined
+                        }
+                      </div>  
+                    })
+                }
+              <label className="label mt-1">
+                <Link className="label-text-alt link link-hover font-semibold">
                 Forgot password?
               </Link>
                 </label>
-              </div>
               <div className="form-control mt-2">
               <button className="btn btn-outline btn-primary">Login</button>
                
